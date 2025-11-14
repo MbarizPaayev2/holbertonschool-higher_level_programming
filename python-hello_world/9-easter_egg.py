eb786bf8ec239f46292d59d1d6e5b5b10a061959
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-14 14:55:54 +0000

    The Zen of Python

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,2 @@
-#!/usr/bin/python
-print("The Zen of Python, by Tim Peters Beautiful is better than ugly.
-Beauti")
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters Beautiful is better than ugly.Beauti")

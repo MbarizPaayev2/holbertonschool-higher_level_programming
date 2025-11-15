73b82ec8194079005280cfc8b48037565ed26d48
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-15 19:16:29 +0000

    this is standart commit for task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def islower(c):
+    """Checks if a character is lowercase"""
     num = ord(c)
-    if 97 < = num < = 122:
+    if 97 <= num <= 122:
         return True
+    return False
+
+# Test bloku (sınaq üçün, lazım deyilsə silinə bilər)
+if __name__ == "__main__":
+    char = input("Bir simvol daxil et: ")
+    if islower(char):
+        print(f"{char} => lower")
     else:
-        return False
+        print(f"{char} => not lower")
+

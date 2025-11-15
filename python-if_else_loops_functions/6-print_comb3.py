62bd016c9624c45116b4ebfb419fa93ee7820426
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-15 18:34:56 +0000

    this is standart commit for task

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if j < i:
+            continue
+        elif i != j:
+            print("{}{}".format(i, j), end=", ")
+    if i == 8 and j == 9:
+        print("{}{}".format(i, j))

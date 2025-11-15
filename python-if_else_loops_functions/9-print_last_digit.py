08f0e1910848d2a60d52e041dbbc95c43bf96e21
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-15 21:10:01 +0000

    this is standart commit for task

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
     number = number % 10
-    print("{:d}".format(print_last_digit(number)))
+    print("{:d}".format(number))
     return number

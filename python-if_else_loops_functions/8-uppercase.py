9d6fab63cd8ad66cd15b58cac4f841b64cf13020
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-15 20:30:55 +0000

    this is standart commit for task

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def uppercase(s):
     for i in s:
         num = ord(i)
         if 97 <= num <= 122:
             i = chr(num - 32)
-        print("{}".format(i), end="")   
+        print("{}".format(i), end="")
+
 
 if __name__ == "__main__":
     s = input()
     uppercase(s)
-

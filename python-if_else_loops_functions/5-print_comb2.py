2f87e61a6037387491883af23e288dbf87c401a7
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-15 18:00:32 +0000

    this is standart commit for task

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print(f"{i:02d}", end=", ")
-    elif i == 99:
+    if i == 99:
         print(i, end="")
     else:
-        print(i, end=", ")
+        print(f"{i:02d}", end=", ")

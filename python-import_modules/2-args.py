a86dc9f938c7d5dbf9bd8719dbc08799bcd30527
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-17 16:14:32 +0000

    this is standart commit for task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 import sys
-a = sys.argv[1:]
-if len(a) == 0:
-    print("0 arguments.")
-elif len (a) == 1:
-    print("1 argument:")
-else:
-    print(len(a), "arguments:")
-for k,i in enumerate(a, 1):
-    print(f"{k}: {i}")
+
+def main():
+    a = sys.argv[1:]
+
+    if len(a) == 0:
+        print("0 arguments.")
+    elif len(a) == 1:
+        print("1 argument:")
+    else:
+        print(f"{len(a)} arguments:")
+
+    for k, i in enumerate(a, 1):
+        print(f"{k}: {i}")
 
 if __name__ == "__main__":
     main()
+

2f7cc93d03e74f75c39247c96e2b25a9ee454965
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-18 15:22:24 +0000

    standart commit for task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     res = ""
     for i in my_string:
         if i == "c" or i == "C":
-            res +=""
+            res += ""
         else:
             res += i
-    return res 
+    return res

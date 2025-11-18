7a306d1be5012d4da3b3701da90b644effe2afd8
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-18 16:30:00 +0000

    standart commit for task

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        row = len(matrix)
+        row = len(matrix[[]])
         for row in matrix:
-            print("{:d}".format(row))
+        print(" ".join("{:d}".format(num) for num in row))
+

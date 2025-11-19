877553c16acdde4a648a47ecb2614da9da694beb
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-19 13:41:23 +0000

    this is standart commit for task

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    new = str(tuple_a)
+    new1 = str(tuple_b)
+    return (new[0] + new1[0], new[1] + new1[1])
+    
+def str(t=()):
+    if len(tuple_a) == 2:
+        n = t[0],t[1]
+    elif len(tuple_a) < 2:
+        n = t[0],0
+    elif len(tuple_a) > 2:
+        n = t[0],t[1]
+    return n;     

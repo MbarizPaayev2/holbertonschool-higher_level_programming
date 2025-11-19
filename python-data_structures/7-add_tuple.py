09b984ca08aeca9ba6235c1aec92ddb6dba8571c
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-19 13:53:15 +0000

    this is standart commit for task

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,17 +1,12 @@
-#!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    ta = resolve(tuple_a)
-    tb = resolve(tuple_b)
-    return (ta[0] + tb[0], ta[1] + tb[1])
-
-
-def resolve(t=()):
-    tlen = len(t)
-    if tlen == 0:
-        tnew = 0, 0
-    elif tlen == 1:
-        tnew = t[0], 0
-    else:
-        tnew = t[0], t[1]
-    return tnew
+    new = f(tuple_a)
+    new1 = f(tuple_b)
+    return (new[0] + new1[0], new[1] + new1[1])
+    
+def f(t=()):
+    if len(t) == 2:
+        return(t[0],t[1])
+    elif len(t) < 2:
+     return( t[0],0)
+    elif len(t) > 2:
+        return (t[0],t[1])

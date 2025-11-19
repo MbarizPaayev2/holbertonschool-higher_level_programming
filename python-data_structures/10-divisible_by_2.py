5b027d72ac2b897c1e39aafaaa26030ee5065e96
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-19 14:41:40 +0000

    this is standart commit for task

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-   for i in my_list:
+    new = []
+    for i in my_list:
        if my_list[i] % 2 == 0:
-           return True
+           new.append(True)
        else:
-           return False
+           new.append(False)

9635bb29cec89a9ab9a9380c34a7ab6b556d22c2
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-19 14:12:20 +0000

    this is standart commit for task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    li = len(sentence)
-    f = sentence[0]
-    if f == "":
-        f = None
-    return li, f
+    if len(sentence)  == 0:
+        return None
+    else:
+        return len(sentence), sentence[0]

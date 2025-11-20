8fb2337e8d550abf23afb7bee619ce0c180052f0
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-20 13:32:00 +0000

    this is standart commit  for task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,7 @@ def search_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             i = replace
+            new_list.append(i)
         else:
-
-
+            new_list.append(i)
+    return new_list

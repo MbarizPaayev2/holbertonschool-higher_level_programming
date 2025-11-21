3e99df8f0e87a1532b60e94f987a36d61df3621b
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-21 13:48:44 +0000

    this is standart commit for task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def best_score(a_dictionary):
+    max = a_dictionary[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > max:
+            max = a_dictionary[key]
+        
+    if max == a_dictionary[0]:
+        return None
+    else:
+        return max

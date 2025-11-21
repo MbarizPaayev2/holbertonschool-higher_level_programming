f8e2f5ae5b7d0b8670d58394ebfdc45469243e74
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-21 13:19:49 +0000

    this is standart commit for task

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-     for key in a_dictionary:
-        a_dictionary[value] = a_dictionary[value] * 2
+    #new_dic = {] 
+     for key in a_dictionary.values():
+        a_dictionary[key] = a_dictionary[key] * 2
     return a_dictionary

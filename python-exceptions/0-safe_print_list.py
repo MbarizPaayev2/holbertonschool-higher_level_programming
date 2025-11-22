8a5fab10ce2880debe44d24c925a751cadd9b61d
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-22 10:33:51 +0000

    this is standart commit for task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     couter = 0
     try:
-     for i in range(x):
-        print(my_list[i], end="")
-        couter = couter + 1
+        for i in range(x):
+            print(my_list[i], end="")
+            couter = couter + 1
     except IndexError:
         pass
     print()
     return couter
-

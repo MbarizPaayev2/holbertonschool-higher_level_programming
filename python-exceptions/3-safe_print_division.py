aa52b89f50b2fd0a3f20b5a8b9fda57dc836984b
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-22 11:42:38 +0000

    this is standart commit for task

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = a / b
-        print("{}".format(res))
+        res =  a / b
+        return res
     except ZeroDivisionError:
         res = None
     finally:
-        print("Inside result:", res)
+        print("{}".format("Inside result:"), res)

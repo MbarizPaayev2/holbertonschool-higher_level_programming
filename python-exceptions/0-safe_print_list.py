95779b9650854c45261bcaca8268c85d11be3231
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-22 10:32:24 +0000

    this is standart commit for task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in my_list:
-        print(i)
+    couter = 0
+    try:
+     for i in range(x):
+        print(my_list[i], end="")
+        couter = couter + 1
+    except IndexError:
+        pass
+    print()
+    return couter
+

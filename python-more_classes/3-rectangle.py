f6a420cd57a86277915cb1082d0abf37985ae5dc
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-24 18:23:41 +0000

    this is standart commit for task

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    def __str__(self):
+
+def __str__(self):
         """return str"""
         str = ""
         if self.__width == 0 or self.__height == 0:

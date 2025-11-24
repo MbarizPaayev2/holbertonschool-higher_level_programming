ab79db6e205754e7a603297268feb97ddafc0fd1
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-24 19:31:16 +0000

    this is standart commit for task

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -50,11 +50,10 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
     def __str__(self):
         """return str"""
-        str = ""
         if self.__width == 0 or self.__height == 0:
-            return str
-        for _ in range(self.__height):
-            str = print("#" * self.__width)
-        return str
+            return ""
+        l = "#" * self.__width
+        return "\n".join([l] * self.__height)
+
     def __repr__(self):
         return "Rectangle({self.__width}, {self.__height})"

7f0d0551b9355a425a99cb41a3ec252b9a98f724
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-24 20:09:51 +0000

    this is standart commit for task

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -4,11 +4,14 @@
 
 class Rectangle:
     """rectangle class"""
-    
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """Initialize Rectangle """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -54,9 +57,11 @@ class Rectangle:
             return ""
         string = "#" * self.__width
         return "\n".join([string] * self.__height)
+    print_symbol = "#"
 
     def __repr__(self):
         return (f"Rectangle({self.__width}, {self.__height})")
-    
+
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1

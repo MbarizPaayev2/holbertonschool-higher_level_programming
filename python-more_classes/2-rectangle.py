4a44f1d6c01523acc3d6e40753a8faf208beca27
Mubariz Pashayev <109411854+MbarizPaayev2@users.noreply.github.com>
2025-11-24 22:05:35 +0400

    this is standart commit
    Fix indentation and correct return statement in perimeter method.

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -47,4 +47,5 @@ class Rectangle:
         """return height """
           if self.__height == 0 or self.__width == 0:
             return 0
-        return 2*(self.__width + self.__height)
+         """return height """
+         return 2*(self.__width + self.__height)

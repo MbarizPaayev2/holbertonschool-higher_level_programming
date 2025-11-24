f74e939bd1a615606e595df7d2a7f1d9320130c6
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-24 15:41:31 +0000

    this is standart commit for task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,13 +7,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """this is document for"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """this is document for """
-    return self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -22,18 +22,18 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-    self.__width = width
+        self.__width = value
 
     @property
     def height(self):
         """return value"""
-    return self.__height
+        return self.__height
 
-    @height_setter
+    @height.setter
     def height(self, value):
         """return height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-    self.__width = width
+        self.__height = value

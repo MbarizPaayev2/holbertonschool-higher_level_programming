b3ed893fd7e2fe4351af2c9d2de2ea132091c9ad
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-25 16:04:16 +0000

    this is standart commit for task

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,10 +3,10 @@
 
 class MyList(list):
     """this is document"""
-        def __init__ (self, list = []):
-                self.list = list
+    def __init__ (self, list = []):
+        self.list = list
  """this is doucment"""
 
-        def print_sorted(self):
-             self.sort()
-             print(self)
+    def print_sorted(self):
+        self.sort()
+        print(self)

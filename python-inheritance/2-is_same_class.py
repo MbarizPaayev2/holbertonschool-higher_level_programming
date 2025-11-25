e7fdf4b24b4cdccf052a079a32f8590fa249ca5b
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-25 16:19:38 +0000

    this is standart commit for task

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """this is also"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, type(a_class)):
         return True
     else:
         return False

b2727498c81ee082f157f390ed1183b4f1773837
Your Name <you@example.com>
2025-11-27 01:15:25 +0400

    this is standart commit task

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """this is docstr"""
-    with open(filename, "w", encoding="utf-8") as json_file:
+    with open(filename, "r", encoding="utf-8") as json_file:
         return json.load(json_file)

2529c74372d491f0f74bc50f263ea9fe25488a78
Your Name <you@example.com>
2025-11-27 00:58:44 +0400

    this is standart commit task

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """this is also docstr"""
-    json.dumps(my_obj)
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(my_obj)
+    with open(filename, "w", encoding="utf-8") as json_file:
+        return json.dump(my_obj, json_file, indent=4)

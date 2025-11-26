610266cff56cf595c13f530e6ef0dd96e2f4b39e
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-26 13:57:20 +0000

    this is standart commit for task

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """this is document"""
-BaseGeomatry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-     """this is document"""
-    
+
     def __init__(self, width, height):
         """this is document"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
         self.__width = width
-        self.__height = height
-        integer_validator("width", self.__width)
-        integer_validator("height", self.__height)
+        Ãself.__height = height

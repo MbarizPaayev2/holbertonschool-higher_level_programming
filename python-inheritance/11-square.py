c2276d0feba9ff115df34b981e4a2d21f780d520
Your Name <you@example.com>
2025-11-26 23:20:54 +0400

    this is standart commit task

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,7 +8,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """this is document"""
         self.integer_validator("size", size)
-        super().__init__(size,size)
+        super().__init__(size, size)
         self.__size = size
+
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"

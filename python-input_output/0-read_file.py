40d4e4cf9e7eedc9a405236c1cdc630bc8f22755
Your Name <you@example.com>
2025-11-27 00:09:37 +0400

    this is standart commit task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""this is document for this task"""
+
+
 def read_file(filename=""):
+    """this is class docstring"""
     with open(filename, "r") as f:
         cont = f.read()
         return cont

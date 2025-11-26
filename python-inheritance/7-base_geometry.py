dca6051f65effc2c51676d9a7c6e070a6f6b9d2b
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-26 11:16:03 +0000

    this is standart commit for task

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this is document"""
+
+
+class BaseGeometry:
+    """this is document"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    """this is document"""
+
+    def integer_validator(self, name, value):
+        if not isinstance(name,int):
+            raise TypeError("<name> must be an integer")
+        elif value <= 0 :
+            raise ValueError("<name> must be greater than 0")
+        self.name = value

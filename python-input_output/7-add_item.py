b6a0becd528a8986cbd53066acd7749e509eacd8
Your Name <you@example.com>
2025-11-27 01:48:24 +0400

    this is standart commit task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,9 +7,9 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 my_list = []
 try:
-     my_list = load_from_json_file("add_item.json")
+    my_list = load_from_json_file("add_item.json")
 except Exception:
-     my_list = []
+    my_list = []
 
 my_list.extend(argv[1:])
 save_to_json_file(my_list, "add_item.json")

c85c7b6e0425c4bc4da44e5c77d9b96bfe817a7e
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-26 11:36:27 +0000

    this is standart commit for task

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
     """this is document"""
 
     def integer_validator(self, name, value):
-        if not isinstance(name,int):
+        if not isinstance(value,int):
             raise TypeError("name must be an integer")
         elif value <= 0 :
             raise ValueError("name must be greater than 0")

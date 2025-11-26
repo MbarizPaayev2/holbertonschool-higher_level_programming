7011ef2b0027191ecbb65426f3da66acbf935f31
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-11-26 13:50:41 +0000

    this is standart commit for task

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 """this is document"""
+BaseGeomatry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry:
-    """this is document"""
-
+class Rectangle(BaseGeometry):
+     """this is document"""
+    
     def __init__(self, width, height):
         """this is document"""
         self.__width = width
         self.__height = height
         integer_validator("width", self.__width)
         integer_validator("height", self.__height)
-
-        raise Exception("area() is not implemented")
-    """this is document"""
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        self.name = value

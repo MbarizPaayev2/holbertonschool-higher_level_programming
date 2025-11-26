693b242c9dcbb55d862ae76916a2c7d9bd22a3fe
Your Name <you@example.com>
2025-11-27 00:36:49 +0400

    this is standart commit task

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is document"""
+
+
+def append_write(filename="", text=""):
+    """this is document"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)

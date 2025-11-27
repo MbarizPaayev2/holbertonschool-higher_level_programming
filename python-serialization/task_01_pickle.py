479a53c6ee86dbec2d8b115fdebbcc6d0eccea15
Your Name <you@example.com>
2025-11-27 22:57:44 +0400

    this is standart commit task

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
+    
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            load = pickle.load(f)
+            return load 

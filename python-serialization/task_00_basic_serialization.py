703f9af12849768878fda4a84a27a73be07b9419
Your Name <you@example.com>
2025-11-27 19:01:44 +0400

    this is standart commit task

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,7 +2,8 @@
 import json
 
 def serialize_and_save_to_file(data, filename):
-    json.dump(filename, data)
+    with open(filename, "w", encoding = "utf-8") as f:
+        json.dump(f, data)
     pass
 
 def load_and_deserialize(filename):

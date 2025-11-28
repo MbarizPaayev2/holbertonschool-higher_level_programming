8f8b4c46034c9634c64b178ecac7ef09093c1ea4
Your Name <you@example.com>
2025-11-28 17:21:51 +0400

    this is standart commit task

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,14 +2,14 @@ import csv
 import json
 
 
-def convert_csv_to_json(csv_file, json_file):
+def convert_csv_to_json(csv_file):
    try:
     data = []
     with open(csv_file, "r", encoding="utf-8") as csvf:
         csv_read = csv.DictReader(csvf)
         for i in csv_read:
             data.append(i)
-    with open(json_file, "w", encoding="utf-8") as jsonf:
+    with open(data, "w", encoding="utf-8") as jsonf:
         json.dump(data, jsonf)
    except FileNotFoundError:
       return False

fb832e985a180bdb8b0c75eff4b0217f08fbd30b
Your Name <you@example.com>
2025-11-28 10:42:34 +0400

    this is standart commit task

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -19,4 +19,4 @@ class CustomObject:
     @classmethod
     def deserialize(cls, filename):
         with open(filename, "rb") as f:
-            return pickle.load(cls, f)
+            return pickle.loads(cls, f)

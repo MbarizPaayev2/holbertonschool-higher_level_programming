7d19e2660662b8e5818751e05300e61e5de8ae36
Your Name <you@example.com>
2025-11-29 01:02:32 +0400

    this is standart commit task

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
 -- this is comment 
-SELECT score FROM second_table WHERE IN (SELECT count(score) as number FROM second_table)  
-ORDER BY score DESC;
+SELECT score FROM second_table WHERE IN (SELECT count(score) as number FROM second_table) ORDER BY score DESC;

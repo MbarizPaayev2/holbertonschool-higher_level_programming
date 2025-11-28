1408ff1a4300f943646cc4696dd4ae079dcf5213
Your Name <you@example.com>
2025-11-28 20:51:02 +0400

    this is standart commit task

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
-    score INT
-)
+    score INT;
+);
 INSERT INTO second_table VALUES(id = 1, name = “John”, score = 10);
 INSERT INTO second_table VALUES(id = 2, name = “Alex”, score = 3);
 INSERT INTO second_table VALUES(id = 3, name = “Bob”, score = 14);

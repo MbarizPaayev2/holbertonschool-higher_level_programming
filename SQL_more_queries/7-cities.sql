e6e61fe91b1b89952999fd76401e6170876c7d45
Your Name <you@example.com>
2025-11-29 21:11:39 +0400

    this is standart commit task

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
-    state_id int 
-    name VARCHAR(256), NOT NULL 
+    state_id INT,
+    name VARCHAR(256) NOT NULL 
 );

1db2162a1cc00b5a158ced3aab995a906c760784
Your Name <you@example.com>
2025-11-29 18:43:42 +0400

    this is standart commit task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- this is commet
-SHOW GRANT FOR 'user_0d_1' @ 'localhost';
-SHOW GRANT FOR  'user_0d_2' @ 'localhost';
+SHOW GRANT FOR 'user_0d_1' @ '127.0.0.1';
+SHOW GRANT FOR  'user_0d_2' @ '127.0.0.1';

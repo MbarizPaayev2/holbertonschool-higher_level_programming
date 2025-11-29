78232fe8b7a1937ce462827a74b2dc164d0b18ec
Your Name <you@example.com>
2025-11-29 21:32:52 +0400

    this is standart commit task

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 -- THIS IS CODE 
 SELECT id, name 
 FROM cities 
-WHERE state.id 
-IN(SELECT id FROM states WHERE name = 'California') 
+WHERE state.id IN( 
+SELECT id 
+FROM states 
+WHERE name = 'California'
+)
 ORDER BY id ASC;

11c50957bb0e44cb007385f53dabdd5f4c85edb9
Your Name <you@example.com>
2025-11-29 20:01:17 +0400

    this is standart commit task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
  -- ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- FLUSH PRIVILEGES; 
 CREATE USER IF EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-SHOW  GRANTS FOR 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+SHOW  GRANTS FOR 'user_0d_1'@'localhost';

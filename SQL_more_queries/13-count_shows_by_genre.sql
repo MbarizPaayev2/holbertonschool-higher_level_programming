5f307f4d111600c9e856bf02e4bddf7d70897cc2
Your Name <you@example.com>
2025-11-30 14:13:39 +0400

    this is standart commit task

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- this is comment 
-SELECT tv_genres.genres_name AS genre COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-HAVING number_of_show > 0
-ORDER BY number_of_show DESC;
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;

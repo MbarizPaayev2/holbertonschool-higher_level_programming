7cbb6cfa46a07062887110713f12385eba40afcc
Your Name <you@example.com>
2025-11-30 14:27:18 +0400

    this is standart commit task

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- this is commnet 
 SELECT tv_shows.title FROM tv_shows 
-JOIN tv_genres ON tv_shows.title = tv_genres.name
-WHERE title = Dexter
-ORDER BY tv_shows.title;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = Dexter
+ORDER BY tv_genres.name;

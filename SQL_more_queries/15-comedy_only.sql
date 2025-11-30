34cdef6b93936c9c7b0e715eb4dcbd1a44a02c5e
Your Name <you@example.com>
2025-11-30 14:37:07 +0400

    this is standart commit task

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,4 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_show.name ASC;

1d095d616e384c1c9523f68caf718f553c6cc307
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-01 15:46:15 +0000

    this is standart commit for task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""this is document"""
+"""This module str """
 import urllib.request
+    url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen(url) as response:
         r = response.read()
         print("Body response:")
         print("type: {}$".format(type(r)))
         print("- content: {}$".format(r))
-        print("- utf8 content: OK$")
+        print("- utf8 content: {}$".format(r.decode('utf-8')))

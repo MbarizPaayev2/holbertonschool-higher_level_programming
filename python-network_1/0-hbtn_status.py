bee62ffc6913be9a1d6708fb8cb2d33585a2bd16
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-01 15:51:53 +0000

    this is standart commit for task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""This module str """
+"""This module fetches status from a URL and prints it."""
 import urllib.request
-  if __name__ == "__main__":
-      url = "https://intranet.hbtn.io/status"
-      with urllib.request.urlopen(url) as response:
-          r = response.read()
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        r = response.read()
         print("Body response:")
         print("type: {}$".format(type(r)))
         print("- content: {}$".format(r))

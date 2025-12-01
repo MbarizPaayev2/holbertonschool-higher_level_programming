83877e331c6b2885d852ac7959ee68bfddd62709
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-01 15:41:56 +0000

    this is standart commit for task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         r = response.read()
         print("Body response:")
-        print("type: {}$".format(type(r))
+        print("type: {}$".format(type(r)))
         print("- content: {}$".format(r))
         print("- utf8 content: OK$")

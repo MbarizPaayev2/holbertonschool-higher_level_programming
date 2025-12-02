b95c5f95f9d9a49e3b791adb9292dc5a6dfc7b2e
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-02 19:20:55 +0000

    this is standart commit for task

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is docstr"""
+
+import sys
+import requests
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        response = requests.post(url, data={"q": q})
+    try:
+        data = response.json()
+        if data == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+    except Exception:
+        print("Not a valid JSON")

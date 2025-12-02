320665ee86220983a75dae7db36be8081f80d816
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-02 14:43:20 +0000

    this is standart commit for task

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    data = urllib.parse.urlencode( {"email": email} ).encode("utf-8")
     req = urllib.request.Request(url, method="POST", data = data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().encode("utf-8"))
+        print(response.read().decode("utf-8"))

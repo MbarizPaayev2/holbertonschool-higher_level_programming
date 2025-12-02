c477eda32ebdb181b71173a02402b9649842b823
Your Name <you@example.com>
2025-12-02 18:23:47 +0400

    this is standart commit task

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
 
+import urllib.parse
 import urllib.request
 import sys
-
-url = sys.argv[1]
-email = sys.argv[2]
-#req = urllib.request.Request(url, method="POST" )
-with urllib.request.urlopen(url) as response:
-    read = response.read().decode("utf-8")
-print("Email: {}".format(email))
- 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email:", email})
+    req = urllib.request.Request(url, method="POST", data = data)

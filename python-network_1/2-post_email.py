63ac59c07f1a62094028c7de063a686bb42f42d9
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-02 14:56:05 +0000

    this is standart commit for task

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+"""
+Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
 
 import urllib.parse
 import urllib.request
 import sys
+
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode( {"email": email} ).encode("utf-8")
-    req = urllib.request.Request(url, method="POST", data = data)
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    req = urllib.request.Request(url, data=data, method="POST")
+
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))

af3f922ea9d2194e1c87f2b17f52cfe43ec50d87
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-04 18:08:11 +0400

    commit

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,17 +6,21 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 users = {}
 
+
 @app.route("/", methods=["GET"])
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route("/data", methods=["GET"])
 def get_data():
     return jsonify(list(users.keys()))
 
+
 @app.route("/status", methods=["GET"])
 def status():
     return "OK"
+
 
 @app.route("/users/<username>", methods=["GET"])
 def get_user(username):
@@ -25,13 +29,13 @@ def get_user(username):
 
     return jsonify({"error": "User not found"}), 404
 
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
     if not request.is_json:
         return jsonify({"error": "Invalid JSON"}), 400
 
     data = request.get_json()
-
     username = data.get("username")
 
     if not username:
@@ -45,11 +49,3 @@ def add_user():
         "age": data.get("age"),
         "city": data.get("city"),
     }
-    return jsonify({
-        "username": username,
-        **users[username]
-    }), 201
-
-
-if __name__ == "__main__":
-    app.run()

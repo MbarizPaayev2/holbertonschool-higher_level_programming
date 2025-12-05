2091a09a768e6612f55d2d88569bf106ffa2ba3f
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2025-12-05 12:46:57 +0000

    this is standart commit for task

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
-import os 
-import json 
+import json
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/items")
 def items():
-    item = []
+    items_list = []
+    json_file = "items.json"
 
-    json_file = 'items.html'
     if os.path.exists(json_file):
         try:
-            with open(json_file, "r") as f:
-                file = json.load(f)
-                items = file.get('items.html', [])
-        except Exception as e :
-                print(f"Error reading JSON file: {e}")
-    else:
-        print(f"{json_file} not found.")
-return render_template('items.html', items=items_list)
+            with open(json_file, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                items_list = data.get("items", [])
+        except Exception as e:
+            print(f"Error reading JSON file: {e}")
+
+    return render_template("items.html", items=items_list)
+
 
 if __name__ == "__main__":
     app.run()
